51d1f576f9d64f886de2785408d854f44e3484f9
Juan A <30204147+jaas666@users.noreply.github.com>
2022-04-27 12:22:12 -0500

    Add some device methods

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
+// TODO Add validations
+
 require("dotenv").config();
 const express = require("express");
-const { MongoDBNamespace, ObjectID } = require("mongodb");
+const { ObjectId } = require("mongodb");
 const mongo = require("mongodb").MongoClient;
-const ObjectId = require("mongodb").ObjectId;
 const app = express();
 const url = process.env.URL;
 
-  let db;
+let db;
 
 mongo.connect(
   url,
@@ -19,70 +20,74 @@ mongo.connect(
       console.error(err);
       return;
     }
-    db = client.db("BatteryLvL");
-    users = db.collection("users-juan");
+    db = client.db(process.env.DB);
+    users = db.collection(process.env.COLLECTION);
   }
 );
 app.use(express.json());
 
 // Users Section
-
-
-
+// [x] List all owner records
 app.get("/users", (req, res) => {
-//  users.find({}).project({firstName:1, lastName:1, email:1}).toArray((err, items) => {
-    users.find({}).toArray((err, items) => {
-
+  users.find().toArray((err, items) => {
     if (err) {
       console.error(err);
       res.status(500).json({ err: err });
       return;
     }
-    res.status(200).json({ users: items });
+    res.status(200).json(items);
   });
 });
 
-app.post("/users/adduser", async (req, res) => {
-  const firstName = req.body.firstname;
-  const lastName = req.body.lastname;
-  const email = req.body.email;
-  //const dbEmail = await users.findOne({ email: email }, {email:1});
-console.log(`The email is ${dbEmail}`)
-  if (dbEmail !== null){
-    
-    users.insertOne(
-      { 
-        firstName: firstName,
-        lastName: lastName,
-        email: email
-      },
-      (err, result) => {
-        if (err) {
-          console.error(err);
-          res.status(500).json({ err: err });
-          return;
-        }
-        console.log(result);
-        res.status(200).json({ ok: true });
-      }
-    );
-  }
- 
+// [x] Retrieve an owner
+app.get("/users/:userId", (req, res) => {
+  const userId = req.params["userId"].toString();
+  users.find({ _id: ObjectId(userId) }).toArray((err, items) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ err: err });
+      return;
+    }
+    res.status(200).json(items);
+  });
 });
 
+// [x]: Create a new Owner
+app.post("/users/adduser", (req, res) => {
+  const { firstName, lastName, email } = req.body;
+
+  users.insertOne(
+    {
+      firstName,
+      lastName,
+      email,
+    },
+    (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ err: err });
+        return;
+      }
+      console.log(result);
+      res.status(200).json(result);
+    }
+  );
+});
+
+// [x]: Update the owner
 app.post("/users/edituser/:id", (req, res) => {
-  const id = req.params["id"].toString();;
-  console.log(req.body);
+  const id = req.params["id"].toString();
   users.updateOne({ _id: ObjectId(id) }, { $set: req.body }, (err, result) => {
     if (err) {
       console.log(err);
       res.status(500).json({ err: err });
       return;
     }
-    res.status(200).json({ result });
+    res.status(200).json(result);
   });
 });
 
+// [x]: Delete an owner.
 app.delete("/users/deleteuser/:id", (req, res) => {
   const id = req.params["id"].toString();
   users.deleteOne({ _id: ObjectId(id) }, (err, result) => {
@@ -91,45 +96,115 @@ app.delete("/users/deleteuser/:id", (req, res) => {
       res.status(500).json({ err: err });
       return;
     }
-    res.status(200).json({ result });
+    res.status(200).json(result);
   });
 });
 
 //Devices Section
 
+// [x]: List all device records
+app.get("/devices/", (req, res) => {
+  users
+    .find()
+    .project({ devices: 1, _id: 0 })
+    .toArray((err, items) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ err: err });
+        return;
+      }
+      res.status(200).json(items);
+    });
+});
+
+// [ ]: Retrieve a device
+// BUG Its returning the whole user record.
+// BUG Need to change path.
+// app.get("/devices/:deviceId", (req, res) => {
+//   const deviceId = req.params["deviceId"].toString();
+//   console.log(deviceId);
+//   users
+//     .find({ "devices.deviceId": ObjectId(deviceId) })
+//     //.project({ devices: 1, _id: 0 })
+//     .toArray((err, items) => {
+//       if (err) {
+//         console.error(err);
+//         res.status(500).json({ err: err });
+//         return;
+//       }
+//       res.status(200).json(items);
+//     });
+// });
+
+// [x]: List all owner device records
 app.get("/devices/:userId", (req, res) => {
   const userId = req.params["userId"].toString();
 
-  users.find({_id: ObjectId(userId)}).project({devices:1, _id:0}).toArray((err, items) => {
+  users
+    .find({ "_id": ObjectId(userId) })
+    .project({ devices: 1, _id: 1 })
+    .toArray((err, items) => {
+      console.log(items)
+      if (err) {
+        console.error(err);
+        res.status(500).json({ err: err });
+        return;
+      }
+      res.status(200).json(items);
+    });
+});
 
+// [x]: Create a new device
+app.post("/devices/adddevice/:userId", async (req, res) => {
+  const userId = req.params["userId"].toString();
+  const { deviceName, deviceLocation } = req.body;
+
+  users.updateOne(
+    { _id: ObjectId(userId) },
+    {
+      $push: {
+        devices: {
+          deviceId: ObjectId(),
+          deviceName,
+          deviceLocation,
+        },
+      },
+    },
+    (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ err: err });
+        return;
+      }
+      res.status(200).json(result);
+    }
+  );
+});
+
+// [ ]: Delete a device and all corresponding device renewal records.
+app.delete("/devices/deletedevice/:deviceId", (req, res) => {
+  const deviceId = req.params["deviceId"].toString();
+  users.updateOne(
+    {},
+    {
+      $pull: {
+        devices: {
+          deviceId: ObjectId(deviceId)
+        },
+      },
+    },
+    (err, result) => {
     if (err) {
-      console.error(err);
+      console.log(err);
       res.status(500).json({ err: err });
       return;
     }
-    res.status(200).json({ users: items });
+    res.status(200).json(result);
   });
 });
 
-app.post("/devices/adddevice/:userId", async (req, res) => {
-  const userId = req.params["userId"].toString();
-  const deviceName = req.body.deviceName;
-  const deviceLocation = req.body.deviceLocation;
-    
-    users.updateOne(
-      {_id:ObjectId(userId)},
-      { "$push": {"devices":{"deviceId":ObjectId(),"deviceName":deviceName,"deviceLocation":deviceLocation}}},
-      (err, result) => {
-        if (err) {
-          console.error(err);
-          res.status(500).json({ err: err });
-          return;
-        }
-        console.log(result);
-        res.status(200).json({ ok: true });
-      }
-    );
-});
 
+// [ ]: Update the device record with a new Renewal record
+// [ ]: List all device renewal records
 
 app.listen(3000, () => console.log("Server ready"));
